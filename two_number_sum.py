1ac35b1fbd99160ca86ecaca106a162c4fef5421
Syed Rifat Ahsan <syed.rifat.ahsan@gmail.com>
2021-03-31 21:27:35 +0600

    Two number solved updated

--- two_number_sum.py
+++ two_number_sum.py
@@ -1,12 +1,31 @@
 input_list = [5, 6, 2, 8, 9, 3, 7, 1]
 target_sum = 10
 
-mapping_dict = dict()
-# result_pair_dict = dict()
-for i in input_list:
-    mapping_dict[i] = ""
-for i in input_list:
-    expected_num = target_sum - i
-    if expected_num in mapping_dict:
-        print(i, expected_num)
-        del mapping_dict[i]
+
+def solve_two_num_sum1(input_list, target_sum):
+    mapping_dict = dict()
+    for i in input_list:
+        expected_num = target_sum - i
+        if expected_num in mapping_dict:
+            print(i, expected_num)
+        else:
+            mapping_dict[i] = None
+
+
+def solve_two_num_sum2(input_list, target_sum):
+    left_pointer = 0
+    right_pointer = len(input_list) - 1
+    input_list.sort()
+    while left_pointer != right_pointer:
+        current_sum = input_list[left_pointer] + input_list[right_pointer]
+        if current_sum == target_sum:
+            print(input_list[left_pointer], input_list[right_pointer])
+            right_pointer -= 1
+            left_pointer += 1
+        elif current_sum > target_sum:
+            right_pointer -= 1
+        else:
+            left_pointer += 1
+
+
+solve_two_num_sum2(input_list, target_sum)
