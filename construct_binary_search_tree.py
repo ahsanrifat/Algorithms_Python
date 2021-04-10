e3f6ed3bddfc2e484852aa6645e86f4f0cb923f2
Syed Rifat Ahsan <syed.rifat.ahsan@gmail.com>
2021-04-10 15:37:16 +0600

    Added Algorithm Closest Value In The BST Search

--- construct_binary_search_tree.py
+++ construct_binary_search_tree.py
@@ -6,7 +6,7 @@ class Node:
         self.right_child: Node = right_child
 
     def __str__(self):
-        parent = self.parent != None and self.parent.value or None
+        # parent = self.parent != None and self.parent.value or None
         left_child = self.left_child != None and self.left_child.value or None
         right_child = self.right_child != None and self.right_child.value or None
         return f"Self->{self.value}, Left->{left_child}, Right->{right_child}"
@@ -21,18 +21,18 @@ def insert_child(root_node: Node, child_value):
     right_node: Node = root_node.right_child
     if child_value < root_node.value:
         if left_node == None:
-            print("Left None")
+            # print("Left None")
             root_node.left_child = Node(child_value, root_node, None, None)
-            print(root_node)
+            # print(root_node)
             return
         else:
             insert_child(root_node.left_child, child_value)
             return
     elif child_value >= root_node.value:
         if right_node == None:
-            print("Right None")
+            # print("Right None")
             root_node.right_child = Node(child_value, root_node, None, None)
-            print(root_node)
+            # print(root_node)
             return
         else:
             insert_child(root_node.right_child, child_value)
@@ -52,7 +52,32 @@ def traverse_bst(root_node: Node):
     traverse_bst(left_node)
 
 
-input_list = [6, 12, 4, 5, 32, 33, 3, 6, 15, 20]
+def find_closest_value_in_bst(parent_node: Node, target_value, closest_value):
+    # closest value updating conditions
+    if parent_node == None:
+        print("Closest Value In The BST is->", closest_value)
+        answer = int(closest_value)
+        return answer
+    elif closest_value == None:
+        closest_value = parent_node.value
+    elif abs(parent_node.value - target_value) < abs(target_value - closest_value):
+        target_parent_dif = abs(parent_node.value - target_value)
+        target_closest_dif = abs(target_value - closest_value)
+        closest_value = parent_node.value
+        # print(
+        #     f"Target-parent->{target_parent_dif}, Target-closest->{target_closest_dif}, Closest->{closest_value}"
+        # )
+    # traversing condition
+    if target_value == parent_node.value:
+        print("Closest Value In The BST is->", closest_value)
+        return target_value
+    elif target_value > parent_node.value:
+        find_closest_value_in_bst(parent_node.right_child, target_value, closest_value)
+    elif target_value < parent_node.value:
+        find_closest_value_in_bst(parent_node.left_child, target_value, closest_value)
+
+
+input_list = [6, 12, 4, 5, 32, 33, 3, 96, 15, 20]
 
 make_root_flag = False
 root_node = None
@@ -62,6 +87,7 @@ for value in input_list:
         make_root_flag = True
     else:
         if root_node != None:
-            print(f"Inserting-----{value}-------")
+            # print(f"Inserting-----{value}-------")
             insert_child(root_node, value)
 # traverse_bst(root_node)
+find_closest_value_in_bst(root_node, 5, None)
